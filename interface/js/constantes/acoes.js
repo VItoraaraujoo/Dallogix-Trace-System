0274a117d9f72c4de014237cbcd95994cf5ca5ed
VItoraaraujoo <hugovitorc10@gmail.com>
2026-09-09 19:07:41 -0300

    Reverter alterações de interface

--- interface/js/constantes/acoes.js
+++ interface/js/constantes/acoes.js
@@ -5,7 +5,6 @@ export const FORM_ACTIONS = new Set([
   "save-dala-edit",
   "submit-product",
   "submit-manifest",
-  "submit-manifest-edit",
   "import-pdf",
   "import-csv",
   "save-occurrence",
